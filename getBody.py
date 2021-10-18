62f2a51620efe683cfacddbcccbc912cba90a141
ManoelCabral1 <54252267+ManoelCabral1@users.noreply.github.com>
2021-10-18 17:14:55 -0300

    Update getBody.py

--- getBody.py
+++ getBody.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import base64
 
 def get_email_body(message):
     """ Retorna o texto do corpo do email.
@@ -10,7 +11,19 @@ def get_email_body(message):
                 if partType == "text/plain" and "attachment" not in part:
                     html_body = part.get_payload()
     else:
-        html_body = message.get_payload(decode=True).decode('utf-8')
+        try:
+            html_body = message.get_payload(decode=True).decode('utf-8')
+        except UnicodeDecodeError:
+            try:
+               html_body = message.get_payload(decode=True).decode("ISO-8859-1")
+            except UnicodeDecodeError:
+                  try:
+                     html_body = message.get_payload(decode=True).decode("ascii")
+                  except UnicodeDecodeError:
+                     try:
+                         html_body = base64.decodestring(message.get_payload())
+                     except UnicodeDecodeError:
+                         pass
     
     # Analisa o html do corpo e extrai o texto
     soup = BeautifulSoup(html_body, 'html.parser')
